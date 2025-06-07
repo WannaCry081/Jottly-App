1233747f09f6575cabf27d8495bbcec64dfd89af
Ly <liraedata59@gmail.com>
2025-06-08 01:22:19 +0800

    ref: ♻️ update useCreateShortenUrl hook to accept UrlRequest type and update mutation handling

--- hooks/useCreateShortenUrl.ts
+++ hooks/useCreateShortenUrl.ts
@@ -1,27 +1,36 @@
 import { useTransition } from "react";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+// Components
+import { toast } from "sonner";
+
+// Services
 import { urlService } from "@/services";
 
-export const useCreateShortenUrl = (url: string) => {
+// Types
+import { UrlRequest } from "@/types/url";
+
+export const useCreateShortenUrl = () => {
   const queryClient = useQueryClient();
   const [isPending, startTransition] = useTransition();
-  const { mutateAsync } = useMutation({
-    mutationFn: async () => await urlService.create(url),
-    onSuccess: (data) => {
+  const { mutate: createShortenUrl } = useMutation({
+    mutationFn: async (request: UrlRequest) => await urlService.create(request),
+    onSuccess: () => {
       startTransition(() => {
         queryClient.invalidateQueries({
           queryKey: ["shortenUrl"],
           exact: true,
         });
       });
-
       // Display success message
+      toast.success("Shortened URL created successfully");
     },
 
     onError: (error) => {
       // Display error message
+      toast.error("An error occurred in creating the shortened URL");
     },
   });
 
-  return { isPending, mutateAsync };
+  return { isPending, createShortenUrl };
 };
