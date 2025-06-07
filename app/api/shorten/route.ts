dc9c7f52429825992e5a35fd2416c943e10e34eb
Ly <liraedata59@gmail.com>
2025-06-08 01:30:40 +0800

    chore: 👽️ hash password before storing in the database

--- app/api/shorten/route.ts
+++ app/api/shorten/route.ts
@@ -1,5 +1,6 @@
 import { nanoid } from "nanoid";
 import { NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
 
 // Database initialization
 import { InitializeDatabase } from "@/data/db";
@@ -8,14 +9,19 @@ import { InitializeDatabase } from "@/data/db";
 import * as schema from "@/data/schemas";
 
 export async function POST(request: Request) {
+  let newPassword: string | undefined;
   const code = nanoid(6);
 
   const db = await InitializeDatabase();
   const { url, password } = await request.json();
 
+  if (password) {
+    newPassword = await bcrypt.hash(password, 10);
+  }
+
   await db.insert(schema.urlSchema).values({
     code: code,
-    password: password ?? null,
+    password: newPassword ?? null,
     originalUrl: url,
   });
 
