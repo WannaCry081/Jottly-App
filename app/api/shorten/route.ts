42c666e457be743afe28fce3561486d8da702984
Ly <liraedata59@gmail.com>
2025-06-08 01:22:51 +0800

    ref: 👽️ add password handling to URL shortening endpoint

--- app/api/shorten/route.ts
+++ app/api/shorten/route.ts
@@ -1,22 +1,25 @@
 import { nanoid } from "nanoid";
 import { NextResponse } from "next/server";
 
+// Database initialization
 import { InitializeDatabase } from "@/data/db";
 
+// Schemas
 import * as schema from "@/data/schemas";
 
 export async function POST(request: Request) {
   const code = nanoid(6);
 
   const db = await InitializeDatabase();
-  const { url } = await request.json();
+  const { url, password } = await request.json();
 
   await db.insert(schema.urlSchema).values({
     code: code,
+    password: password ?? null,
     originalUrl: url,
   });
 
   return NextResponse.json({
-    shortUrl: `${process.env.NEXT_PUBLIC_API_URL}/${code}`,
+    url: `${process.env.NEXT_PUBLIC_API_URL}/${code}`,
   });
 }
