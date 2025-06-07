8ebf2caffb98116789fb062766b5f64ee2f0bf57
Ly <liraedata59@gmail.com>
2025-06-08 01:22:37 +0800

    chore: 🗃️ add password field to urlSchema for enhanced URL security

--- data/schemas/url.ts
+++ data/schemas/url.ts
@@ -3,6 +3,7 @@ import { pgTable, uuid, varchar, text, timestamp } from "drizzle-orm/pg-core";
 export const urlSchema = pgTable("url", {
   id: uuid("id").defaultRandom().primaryKey(),
   code: varchar("code", { length: 10 }).notNull().unique(),
+  password: varchar("password", { length: 255 }),
   originalUrl: text("original_url").notNull(),
   createdAt: timestamp("created_at", { withTimezone: true }).defaultNow(),
 });
