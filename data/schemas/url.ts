8623d0a25ae25754c362e513fb51b298adca4da4
Ly <liraedata59@gmail.com>
2025-06-08 11:26:48 +0800

    chore: 🗃️ add ownerId field to url schema

--- data/schemas/url.ts
+++ data/schemas/url.ts
@@ -2,6 +2,7 @@ import { pgTable, uuid, varchar, text, timestamp } from "drizzle-orm/pg-core";
 
 export const urlSchema = pgTable("url", {
   id: uuid("id").defaultRandom().primaryKey(),
+  ownerId: varchar("owner_id", { length: 50 }).notNull(),
   code: varchar("code", { length: 10 }).notNull().unique(),
   password: varchar("password", { length: 255 }),
   originalUrl: text("original_url").notNull(),
