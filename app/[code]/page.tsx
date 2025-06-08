e549eec19d22c58ca3495c68248f0dda605bcb4c
Ly <liraedata59@gmail.com>
2025-06-08 15:32:52 +0800

    feat: ✨ implement Page component to render UrlRedirectPage with dynamic code handling

--- app/[code]/page.tsx
+++ app/[code]/page.tsx
@@ -1,3 +1,15 @@
-export default async function Page() {
-  return <div></div>;
+import { UrlRedirectPage } from "@/components/url/UrlRedirect";
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ code: string }>;
+}) {
+  const { code } = await params;
+
+  return (
+    <main>
+      <UrlRedirectPage code={code} />
+    </main>
+  );
 }
