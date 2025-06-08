6222677b5c9785464eea7fa92e432fb59025c03c
Ly <liraedata59@gmail.com>
2025-06-08 15:59:14 +0800

    chore: 🔨 add partialUpdate method to urlService for updating URLs

--- services/url.ts
+++ services/url.ts
@@ -7,6 +7,8 @@ export const urlService = {
   list: async (ownerId: string) =>
     axiosInstance.get(`/shorten?ownerId=${ownerId}`),
   retrieve: async (code: string) => axiosInstance.get(`/shorten?code=${code}`),
+  partialUpdate: async (code: string) =>
+    axiosInstance.patch(`/shorten?code=${code}`),
   create: async (request: UrlRequest) =>
     axiosInstance.post("/shorten", request),
 };
