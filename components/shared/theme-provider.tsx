a3e586b50c731ebf942084a5057c5167ede359ec
Ly <liraedata59@gmail.com>
2025-06-08 23:01:18 +0800

    feat: ✨ wrap ThemeProvider with TooltipProvider for enhanced tooltip functionality

--- components/shared/theme-provider.tsx
+++ components/shared/theme-provider.tsx
@@ -3,9 +3,16 @@
 import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 
+// Components
+import { TooltipProvider } from "../ui/tooltip";
+
 export function ThemeProvider({
   children,
   ...props
 }: React.ComponentProps<typeof NextThemesProvider>) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+  return (
+    <NextThemesProvider {...props}>
+      <TooltipProvider>{children}</TooltipProvider>
+    </NextThemesProvider>
+  );
 }
