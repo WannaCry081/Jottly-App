f8c94cec851d4febf084298e0cba2c3d16add82f
Ly <liraedata59@gmail.com>
2025-06-08 11:27:59 +0800

    ref: ♻️ update POST and GET methods to include ownerId in request handling

--- app/api/shorten/route.ts
+++ app/api/shorten/route.ts
@@ -1,5 +1,5 @@
 import { nanoid } from "nanoid";
-import { NextResponse } from "next/server";
+import { NextResponse, type NextRequest } from "next/server";
 import bcrypt from "bcryptjs";
 
 // Database initialization
@@ -7,19 +7,21 @@ import { InitializeDatabase } from "@/data/db";
 
 // Schemas
 import * as schema from "@/data/schemas";
+import { eq } from "drizzle-orm";
 
 export async function POST(request: Request) {
   let newPassword: string | undefined;
   const code = nanoid(6);
 
   const db = await InitializeDatabase();
-  const { url, password } = await request.json();
+  const { ownerId, url, password } = await request.json();
 
   if (password) {
     newPassword = await bcrypt.hash(password, 10);
   }
 
   await db.insert(schema.urlSchema).values({
+    ownerId: ownerId,
     code: code,
     password: newPassword ?? null,
     originalUrl: url,
@@ -28,4 +30,35 @@ export async function POST(request: Request) {
   return NextResponse.json({
     url: `${process.env.NEXT_PUBLIC_API_URL}/${code}`,
   });
+}
+
+export async function GET(request: NextRequest) {
+  const db = await InitializeDatabase();
+  const searchParams = request.nextUrl.searchParams;
+
+  const code = searchParams.get("code");
+  const ownerId = searchParams.get("ownerId");
+
+  if (code) {
+    const response = await db
+      .select()
+      .from(schema.urlSchema)
+      .where(eq(schema.urlSchema.code, code));
+
+    return NextResponse.json({ url: response[0] });
+  }
+
+  if (ownerId) {
+    const response = await db
+      .select()
+      .from(schema.urlSchema)
+      .where(eq(schema.urlSchema.ownerId, ownerId));
+
+    return NextResponse.json({ urls: response });
+  }
+
+  return NextResponse.json(
+    { error: "Invalid request parameters" },
+    { status: 400 }
+  );
 }
