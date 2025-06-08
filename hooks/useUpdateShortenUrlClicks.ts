5a4e934ff943d4b235e140675cea2eb0493ae7f7
Ly <liraedata59@gmail.com>
2025-06-08 19:04:16 +0800

    ref: ♻️ remove toast notifications from useUpdateShortenUrlClicks hook

--- hooks/useUpdateShortenUrlClicks.ts
+++ hooks/useUpdateShortenUrlClicks.ts
@@ -1,9 +1,6 @@
 import { useTransition } from "react";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
-// Components
-import { toast } from "sonner";
-
 // Services
 import { urlService } from "@/services";
 
@@ -19,13 +16,6 @@ export const useUpdateShortenUrlClicks = () => {
           exact: true,
         });
       });
-      // Display success message
-      toast.success("Shortened URL created successfully");
-    },
-
-    onError: (error) => {
-      // Display error message
-      toast.error("An error occurred in creating the shortened URL");
     },
   });
 
