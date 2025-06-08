085e563bd4facf3d079376dbd368198ae7254e62
Ly <liraedata59@gmail.com>
2025-06-08 14:40:24 +0800

    ref: ♻️ enhance Page component layout and integrate dialog for viewing all links

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,23 @@
 import React from "react";
+import { Unlink2, MoveRight, Info } from "lucide-react";
 
 // Components
 import { UrlForm } from "@/components/url/UrlForm";
-import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
-
-// Icons
-import { TriangleAlert, Unlink2 } from "lucide-react";
+import { UrlList } from "@/components/url/UrlList";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
 
 export default async function Page() {
   return (
     <React.Fragment>
-      <header className="max-w-lg mx-auto">
+      <header className="max-w-xl mx-auto">
         <div className="p-4 pt-8 lg:pt-14">
           <span className="inline-flex items-center gap-2 mb-2 md:mb-4">
             <div className="size-8 md:size-10 bg-primary rounded-sm grid place-items-center">
@@ -26,23 +33,41 @@ export default async function Page() {
           </p>
         </div>
       </header>
-      <main className=" max-w-lg mx-auto">
+      <main className=" max-w-xl mx-auto">
         <div className="p-4 py-2 space-y-6">
           <UrlForm />
-
-          <Alert variant="warning" className="shadow-sm">
-            <TriangleAlert />
-            <AlertTitle>Warning: This URL is temporary</AlertTitle>
-            <AlertDescription className="text-sm">
-              This shortened URL will expire in 30 days. Make sure to save or
-              share it before then, as it cannot be extended at this time.
-            </AlertDescription>
-          </Alert>
+          <section className="space-y-4">
+            <div className="leading-tight">
+              <h2 className="text-lg md:text-xl font-medium">Recent Links</h2>
+              <span className="text-sm text-muted-foreground">
+                Access your latest shortened links here.
+              </span>
+            </div>
+            <UrlList limit={4} />
+          </section>
+          <Dialog>
+            <div className="w-full grid place-items-center">
+              <DialogTrigger asChild>
+                <Button variant="link">
+                  <span>View all links</span>
+                  <MoveRight />
+                </Button>
+              </DialogTrigger>
+            </div>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>All Shortened Links</DialogTitle>
+                <DialogDescription className="text-sm text-muted-foreground">
+                  Browse all your shortened links below.
+                </DialogDescription>
+              </DialogHeader>
+              <div>
+                <UrlList />
+              </div>
+            </DialogContent>
+          </Dialog>
         </div>
       </main>
-      <footer className="text-center text-xs text-muted-foreground py-4 absolute bottom-0 flex items-center justify-center w-full">
-        © 2025 WannaCry081
-      </footer>
     </React.Fragment>
   );
 }
