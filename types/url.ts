f9865bb05ce6a26c2821ca522d6fb7caa6e4e103
Ly <liraedata59@gmail.com>
2025-06-15 16:37:39 +0800

    chore: 🏷️ add Url type definition for enhanced URL data structure

--- types/url.ts
+++ types/url.ts
@@ -3,3 +3,13 @@ export type UrlRequest = {
   url: string;
   password?: string;
 };
+
+export type Url = {
+  id: string;
+  ownerId: string;
+  originalUrl: string;
+  code: string;
+  password: string | null;
+  createdAt: Date;
+  clicks: number;
+};
