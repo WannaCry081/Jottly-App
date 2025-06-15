cf3b83e02952d59a2e53ba833dfae49eb1e5bdc8
Ly <liraedata59@gmail.com>
2025-06-15 18:17:45 +0800

    chore: 🔧 add NEXT_PUBLIC_APP_NAME environment variable

--- types/env.d.ts
+++ types/env.d.ts
@@ -1,6 +1,7 @@
 namespace NodeJS {
   interface ProcessEnv {
     NEXT_PUBLIC_ENV: "development" | "production";
+    NEXT_PUBLIC_APP_NAME: string;
     NEXT_PUBLIC_API_URL: string;
     NEXT_PUBLIC_DATABASE_URL: strign;
     NEXT_PUBLIC_SECRET_KEY: string;
