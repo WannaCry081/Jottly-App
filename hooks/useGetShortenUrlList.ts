22889f1ba1666178d1ec911b1eb03e7b104490a3
Ly <liraedata59@gmail.com>
2025-06-15 16:37:13 +0800

    feat: ✨ update useGetShortenUrlList hook to include response typing and enhance query structure

--- hooks/useGetShortenUrlList.ts
+++ hooks/useGetShortenUrlList.ts
@@ -1,11 +1,16 @@
 import { useQuery } from "@tanstack/react-query";
 import { urlService } from "@/services";
 
+import { Response } from "@/types/response";
+import { Url } from "@/types/url";
+
 export const useGetShortenUrlList = (ownerId: string) => {
-  const { data, error, isLoading, isError } = useQuery({
-    queryKey: ["shortenUrl"],
-    queryFn: async () => await urlService.list(ownerId),
-    select: (data) => data.data,
+  const { data, error, isLoading, isError } = useQuery<Response<Url[]>, Error>({
+    queryKey: ["shortenUrl", ownerId],
+    queryFn: async () => {
+      const response = await urlService.list(ownerId);
+      return response.data;
+    },
     enabled: !!ownerId,
   });
 
