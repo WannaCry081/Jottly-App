0246d09e69cd96ac9190bac8879decd1389fd8d7
Ly <liraedata59@gmail.com>
2025-06-22 12:49:13 +0800

    feat: ✨ add ESLint configuration for improved code quality

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    dirs: [
+      "app",
+      "data",
+      "constants",
+      "config",
+      "components",
+      "hooks",
+      "lib",
+      "services",
+      "utils",
+      "types",
+    ],
+  },
 };
 
 export default nextConfig;
